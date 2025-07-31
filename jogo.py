b3b8e2eb22a746f78fccfcbec319e4c82cefdb6e
Matheus359660 <goncalves.santos.matheus1601@escola.pr.gov.br>
2025-07-31 12:17:54 +0000

    primeira parte do jogo

---
+++ jogo.py
@@ -0,0 +1,10 @@
+import random
+print("######################################")
+print("#       Jogo de adivinhação          #")
+print("#            Matheus                 #")
+print("######################################")
+print("#         bem vindo ao game 👺🪽     #")
+
+numeroSecreto= random.randrange(0,100)
+totalTentativas = 0
+pontos = 100
